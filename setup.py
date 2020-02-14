5b5f88c8033c02fe7a637deb1a7a241472c29837
Matt Wiley <wileymab@gmail.com>
2020-02-13 22:21:59 -0500

    Added automatic comprehension of requirements file into install_reqs list

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+with open(path.join(here, "README.md"), encoding="utf-8") as readme:
+    long_description = readme.read()
+
+# Collect the contents of 'requirements.txt' as a list for use in the setup definition
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as requirements:
+    requirements_contents = requirements.read()
+    requirements_list = [
+        line
+        for line in requirements_contents.split("\n")
+        if len(line) > 0 and not line.startswith("#")
+    ]
 
 setup(
     name="app",
@@ -18,13 +27,7 @@ setup(
     classifiers=[],
     keywords="",
     packages=find_packages(exclude=["tests"]),
-    package_data={
-        "app": []
-    },
-    install_requires=[],
-    entry_points={
-        "console_scripts": [
-            "app=app.__main__"
-        ],
-    },
+    package_data={app_module: []},
+    install_requires=requirements_list,
+    entry_points={"console_scripts": [f"app_run=app.__main__"],},
 )
